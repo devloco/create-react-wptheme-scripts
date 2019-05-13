8abf1568d634245bd2d0d1c36b6f09954c50fcef
Joseph Handley <handleyj@hotmail.com>
2019-05-13 00:55:07 -0500

    3.0.1 accepted changes

--- packages/react-scripts/scripts/eject.js
+++ packages/react-scripts/scripts/eject.js
@@ -38,9 +38,25 @@ function getGitStatus() {
   }
 }
 
+function tryGitAdd(appPath) {
+  try {
+    spawnSync(
+      'git',
+      ['add', path.join(appPath, 'config'), path.join(appPath, 'scripts')],
+      {
+        stdio: 'inherit',
+      }
+    );
+
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
 console.log(
   chalk.cyan.bold(
-    'NOTE: Create React App 2 supports TypeScript, Sass, CSS Modules and more without ejecting: ' +
+    'NOTE: Create React App 2+ supports TypeScript, Sass, CSS Modules and more without ejecting: ' +
       'https://reactjs.org/blog/2018/10/01/create-react-app-v2.html'
   )
 );
@@ -309,6 +325,11 @@ inquirer
     }
     console.log(green('Ejected successfully!'));
     console.log();
+
+    if (tryGitAdd(appPath)) {
+      console.log(cyan('Staged ejected files for commit.'));
+      console.log();
+    }
 
     console.log(
       green('Please consider sharing why you ejected in this survey:')
